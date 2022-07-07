05263c8a5a119b6d784a680b9241424b0fca2218
lebrancconvas <lebrancconvas@gmail.com>
2022-07-07 07:14:09 +0700

    🔨 Create Router.

--- development/services/api/src/app.ts
+++ development/services/api/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import router from './routes/router';
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello Server'); 
 });
+
+app.use(router);
 
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
